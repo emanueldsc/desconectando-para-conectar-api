11a1679f3c4fc7f67708e08ec5e5503844a454dc
Emanuel Douglas <emanuel.douglas.sc@gmail.com>
2026-05-23 20:57:10 -0300

    Alterando rota padrão da API

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return response()->json([
+        'name' => 'API - Desconectando para Conectar',
+        'status' => 'online',
+        'version' => '1.0'
+    ]);
 });
